95371b2eadcff5de87d2d13c03ac4ba583e76de9
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-05 02:16:16 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 class CoordinatesButton extends React.Component {
+  constructor(props) {
+      super()
+      this.state = {
+        onRecieveCoordinates: props.onRecieveCoordinates
+      }
+  }
+  
   render() {
     
   }
