6c7995f0b4cc54d8867bc508e79ac524186fc2b8
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-05 02:21:19 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -7,7 +7,7 @@ class DelayedButton extends React.Component {
   
   render() {
     return (
-      <button onClick={ this.DelayedButton}>Button 2</button>
+      <button onClick={ this.onDelayedClick }>Button 2</button>
       )
   }
 }
